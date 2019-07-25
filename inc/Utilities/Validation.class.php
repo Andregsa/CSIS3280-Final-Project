252835a99ed96f08ca3bbd5cf97a52268f4dbea3
anushadas <38142919+anushadas@users.noreply.github.com>
2019-07-25 15:13:33 -0700

    Adding Validation Methods

--- inc/Utilities/Validation.class.php
+++ inc/Utilities/Validation.class.php
@@ -1,4 +1,33 @@
 <?php
 //Validate User Input -- Needed for Log In
-
+class Validate($inputs)
+{
+  function validateLogin($inputs)
+  {
+    $errors = array();
+    if(empty($inputs['username']))
+    {
+      $errors = "Please enter a username";
+    }
+    if(empty($inputs['password']))
+    {
+      $errors = "Please enter a password";
+    }
+    return $errors;
+  }
+   function validateSignUp($inputs)
+  {
+    $errors = array();
+    if(empty($inputs['username']))
+    {
+      $errors = "Please enter a username";
+    }
+    if(empty($inputs['password']))
+    {
+      $errors = "Please enter a password";
+    }
+    return $errors;
+  }
+  
+}
 ?>
