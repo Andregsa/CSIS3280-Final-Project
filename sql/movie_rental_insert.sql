de6297422555ccebfa12a89515b85803492753b9
Andregsa <andreg_sa@hotmail.com>
2019-08-01 08:30:49 -0700

    SQL FIX

--- sql/movie_rental_insert.sql
+++ sql/movie_rental_insert.sql
@@ -6,13 +6,13 @@ INSERT INTO User VALUES
   (3,'$2y$10$u.CuPl.igZL6a0M7eSMuqOvPcqpYm9tNUVq7DTp4FW60Orieni68G','marthur@gmail.com', 'Michelle','Arthur', '1999-04-10');
 
 INSERT INTO WatchedMovies VALUES
-  (4,1,3,'2019-04-15',4.3),
-  (5,1,3,'2019-07-15',7.5),
-  (6,2,2,'2019-06-06',10.40);
+  (4,1,"tt0110357",'2019-04-15',4.3),
+  (5,1,"tt4154796",'2019-07-15',7.5),
+  (6,2,"tt7131622",'2019-06-06',10.40);
 
 
 INSERT INTO MyMovies VALUES
-  (1, 1,"IMDBID","The Lion King", "1994", 88,"Animation, Adventure, Drama, Family, Musical","A Lion cub crown prince 
+  (1, 1,"tt0110357","The Lion King", "1994", 88,"Animation, Adventure, Drama, Family, Musical","A Lion cub crown prince 
   is tricked by a treacherous uncle into thinking he caused his father's death and flees into exile in 
   despair, only to learn in adulthood his identity and his responsibilities",
   "https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2J
@@ -75,13 +75,13 @@ INSERT INTO MyMovies VALUES
   
   (21, "Avatar", 2009, 162, "Action, Adventure, Fantasy, Sci-Fi", "A paraplegic Marine dispatched 
   to the moon Pandora on a unique mission becomes torn between following his orders and protecting 
-  the world he feels is his home.","https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",6.8, "Top Rated","tt2283336"),
+  the world he feels is his home.","https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",6.8, "Top Rated","tt0499549"),
   
   (22, "Star Wars: Episode V - The Empire Strikes Back", 1980, 124, "Action, Adventure, Fantasy, Sci-Fi","After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued by Darth Vader.","https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",8.7, "Top Rated","tt0080684" ),
   
   (23, "Avengers Endgame", 2019, 181, "Action, Adventure, Sci-Fi","After the devastating events of 
   Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, 
   the Avengers assemble once more in order to reverse Thanos' actions and restore balance to 
-  the universe.","https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",6.8, "Top Rated","tt2283336" ),
+  the universe.","https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",6.8, "Top Rated","tt4154796" ),
   
   (24, "City of God", 2002,130, "Crime, Drama", "	In the slums of Rio, two kids' paths diverge as one struggles to become a photographer and the other a kingpin.","https://m.media-amazon.com/images/M/MV5BMGU5OWEwZDItNmNkMC00NzZmLTk1YTctNzVhZTJjM2NlZTVmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",8.6, "Top Rated","tt0317248");
